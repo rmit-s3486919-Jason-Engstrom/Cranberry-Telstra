f1e2636a39b33dd0fe421bf74ec98808d5b334f4
Mr4D <a.prabhakara96@gmail.com>
2017-08-27 11:15:20 +1000

    Proposed architecture for website

---
+++ cranberry/assets/parts/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+  <div class="container">
+    <span class="text-muted">© Cranberry, All rights reserved.</span>
+  </div>
+</footer>
---
+++ cranberry/assets/parts/includes.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<!-- Bootstarp CSS -->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/css/sticky-footer.css" rel="stylesheet">
+<!-- Bootstrap theme -->
+<link href="assets/css/bootstrap-theme.min.css" rel="stylesheet">
