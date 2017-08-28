5320daf2abf85ef4514abb8407956aab4df3819c
rmit-s3486919-Jason-Engstrom <s3486919@student.rmit.edu.au>
2017-08-28 05:52:00 +0000

    slight changes to transfer code

--- Raspberry Pi/SENDER CODE (proto one)/SPACES.py
+++ Raspberry Pi/SENDER CODE (proto one)/SPACES.py
@@ -13,9 +13,9 @@ UDP_PORT = 5005
 #GPIO setup
 import RPi.GPIO as GPIO
 import time
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.IN)
+##GPIO.setwarnings(False)
+##GPIO.setmode(GPIO.BOARD)
+##GPIO.setup(11, GPIO.IN)
 
 #Unit details
 device = "slave1"
@@ -26,36 +26,38 @@ import datetime
 
 #Forever loop
 while True:
-    Status=GPIO.input(11)
+##    Status=GPIO.input(11)
+    Status = 1
     if Status:
         #Time stamp creation
         ts= time.time()
         st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
         #capture image
         BaseStringi= 'raspistill -o "/home/pi/Design3_code/SENDER_CODE/'
-        ImgName=st + ' ' + device+'.jpg'
+        ImgName=st + device+'.jpg'
         ImgName_t=ImgName+'"'
         capString= BaseStringi + ImgName_t
-        os.system(capString)
-        time.sleep(8)
+##        os.system(capString)
+##        time.sleep(8)
         #copy file
         BaseString_s='sshpass -p "Pi2017" scp "/home/pi/Design3_code/SENDER_CODE/'
         EndString_s=' pi@192.168.1.1:/home/pi/Blake_receive_code/Receive_folder'
         Complete_String=BaseString_s +ImgName_t+EndString_s
-        os.system(Complete_String)
-        time.sleep(6)
+##        os.system(Complete_String)
+##        time.sleep(6)
         #os.system("""sshpass -p "Pi2017" scp /home/pi/image1.jpg pi@192.168.1.1:/home/pi/Blake_receive_code/Receive_folder""")
         
         #Message creation
         MESSAGE = st +'|'+ device +'|'+ lat +'|'+ lon +'|'+ ImgName
-        #UDP transmission
+##        print MESSAGE
+##        #UDP transmission
         print 'UDP target IP:', UDP_IP
         print "UDP target port:", UDP_PORT
         print "message:", MESSAGE
-
-        sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-        sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-        time.sleep(1)
+##
+##        sock = socket.socket(socket.AF_INET, # Internet
+##                     socket.SOCK_DGRAM) # UDP
+##        sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+##        time.sleep(1)
         
 
