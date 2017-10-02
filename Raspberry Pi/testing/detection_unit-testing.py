36bbfb74bb18df5f50eefd92b7c1b2a333bf8f54
Mr4D <a.prabhakara96@gmail.com>
2017-10-03 04:42:58 +1100

    fix testing code

--- Raspberry Pi/testing/detection_unit-testing.py
+++ Raspberry Pi/testing/detection_unit-testing.py
@@ -37,70 +37,54 @@ bads = 0;
 
 #Forever loops
 for x in range(0,999):
-	# Status=GPIO.input(11)
-	if True:
-		#Time measurements
-		start_time = time.time()
+	#Time measurements
+	start_time = time.time()
+	########################################
+	##           CAPTURE  IMAGE           ##
+	########################################
+	img = '2017-10-01 14:10:01 202481594827183.jpg"'
 
-		#Time stamp creation
-		ts= time.time()
-		st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-		# print("Sent at: " + str(start_time))
+	#Time measurements
+	########################################
+	##     SEND IMAGE TO ACCESS POINT     ##
+	########################################
 
-		########################################
-		##           CAPTURE  IMAGE           ##
-		########################################
-		if ImgName is "":
-			BaseStringi= 'raspistill -h 1080 -w 1440 -o "/home/pi/Pictures/'
-			ImgName=st + ' ' + mac+'.jpg'
-			ImgName_t=ImgName+'"'
+	#Uses SSH
+	BaseString_s='sshpass -p "Pi2017" scp "/home/pi/Pictures/'
+	EndString_s=' pi@192.168.1.1:/home/pi/Pictures'
+	Complete_String=BaseString_s + img+EndString_s
+	os.system(Complete_String)
+	#os.system("""sshpass -p "Pi2017" scp /home/pi/image1.jpg pi@192.168.1.1:/home/pi/Blake_receive_code/Receive_folder""")
 
-			capString= BaseStringi + ImgName_t
-			os.system(capString)#uses capString as a command for the OS to run
+	########################################
+	##            SEND MESSAGE            ##
+	########################################
 
-		#Time measurements
-		time_to_capture_image = start_time - time.time()
+	#Uses TCP
+	# MESSAGE = st +'|'+ device +'|'+ lat +'|'+ lon +'|'+ ImgName
 
-		########################################
-		##     SEND IMAGE TO ACCESS POINT     ##
-		########################################
-
-		#Uses SSH
-		BaseString_s='sshpass -p "Pi2017" scp "/home/pi/Pictures/'
-		EndString_s=' pi@192.168.1.1:/home/pi/Pictures'
-		Complete_String=BaseString_s +ImgName_t+EndString_s
-		os.system(Complete_String)
-		#os.system("""sshpass -p "Pi2017" scp /home/pi/image1.jpg pi@192.168.1.1:/home/pi/Blake_receive_code/Receive_folder""")
-
-		########################################
-		##            SEND MESSAGE            ##
-		########################################
-
-		#Uses TCP
-		# MESSAGE = st +'|'+ device +'|'+ lat +'|'+ lon +'|'+ ImgName
-
-		MESSAGE = st +'|'+ mac +'|'+ deviceName +'|'+ longitude +'|'+ latitude +'|'+ user_id +'|'+ ImgName
+	MESSAGE = st +'|'+ mac +'|'+ deviceName +'|'+ longitude +'|'+ latitude +'|'+ user_id +'|'+ ImgName
 
 
-		#TCP transmission
-		# print 'TCP target IP:', TCP_IP
-		# print "TCP target port:", TCP_PORT
-		# print "message:", MESSAGE
+	#TCP transmission
+	# print 'TCP target IP:', TCP_IP
+	# print "TCP target port:", TCP_PORT
+	# print "message:", MESSAGE
 
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		sock.connect((TCP_IP,TCP_PORT))
-		try:
-			sock.send(MESSAGE)
-			reply = sock.recv(BUFFER_SIZE)
-			goods = goods + 1
-			time_to_receive_reply = time.time() - start_time
-			print(str(time_to_receive_reply))
-		except socket.error, e:
-			# sock.close()
-			bads = bads + 1
-			print e.errno
-		print reply
-		sock.close()
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.connect((TCP_IP,TCP_PORT))
+	try:
+		sock.send(MESSAGE)
+		reply = sock.recv(BUFFER_SIZE)
+		goods = goods + 1
+		time_to_receive_reply = time.time() - start_time
+		print(str(time_to_receive_reply))
+	except socket.error, e:
+		# sock.close()
+		bads = bads + 1
+		# print e.errno
+	print reply
+	sock.close()
 
 		# print(str(x))
 		# print(str(start_time))
