4dd3f6869e6238c624e5ff58386641642c6d6420
rmit-s3486919-Jason-Engstrom <s3486919@student.rmit.edu.au>
2017-10-03 15:06:16 +1100

    spaces -> tabs

--- Raspberry Pi/testing/detection_unit-testing.py
+++ Raspberry Pi/testing/detection_unit-testing.py
@@ -37,75 +37,75 @@ bads = 0;
 
 #Forever loop
 for x in range(0,999):
-    Status=GPIO.input(11)
-    if True:
-        #Time measurements
-        start_time = time.time()
+	Status=GPIO.input(11)
+	if True:
+		#Time measurements
+		start_time = time.time()
 
-        #Time stamp creation
-        ts= time.time()
-        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-        # print("Sent at: " + str(start_time))
+		#Time stamp creation
+		ts= time.time()
+		st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+		# print("Sent at: " + str(start_time))
 
-        ########################################
-        ##           CAPTURE  IMAGE           ##
-        ########################################
+		########################################
+		##           CAPTURE  IMAGE           ##
+		########################################
 
-        BaseStringi= 'raspistill -h 1080 -w 1440 -o "/home/pi/Pictures/'
-        if ImgName is none:
-	        ImgName=st + ' ' + mac+'.jpg'
-	        ImgName_t=ImgName+'"'
+		BaseStringi= 'raspistill -h 1080 -w 1440 -o "/home/pi/Pictures/'
+		if ImgName is none:
+			ImgName=st + ' ' + mac+'.jpg'
+			ImgName_t=ImgName+'"'
 
-        capString= BaseStringi + ImgName_t
-        os.system(capString)#uses capString as a command for the OS to run
+		capString= BaseStringi + ImgName_t
+		os.system(capString)#uses capString as a command for the OS to run
 
-        #Time measurements
-        time_to_capture_image = start_time - time.time()
+		#Time measurements
+		time_to_capture_image = start_time - time.time()
 
-        ########################################
-        ##     SEND IMAGE TO ACCESS POINT     ##
-        ########################################
+		########################################
+		##     SEND IMAGE TO ACCESS POINT     ##
+		########################################
 
-        #Uses SSH
-        BaseString_s='sshpass -p "Pi2017" scp "/home/pi/Pictures/'
-        EndString_s=' pi@192.168.1.1:/home/pi/Pictures'
-        Complete_String=BaseString_s +ImgName_t+EndString_s
-        os.system(Complete_String)
-        #os.system("""sshpass -p "Pi2017" scp /home/pi/image1.jpg pi@192.168.1.1:/home/pi/Blake_receive_code/Receive_folder""")
+		#Uses SSH
+		BaseString_s='sshpass -p "Pi2017" scp "/home/pi/Pictures/'
+		EndString_s=' pi@192.168.1.1:/home/pi/Pictures'
+		Complete_String=BaseString_s +ImgName_t+EndString_s
+		os.system(Complete_String)
+		#os.system("""sshpass -p "Pi2017" scp /home/pi/image1.jpg pi@192.168.1.1:/home/pi/Blake_receive_code/Receive_folder""")
 
-        ########################################
-        ##            SEND MESSAGE            ##
-        ########################################
+		########################################
+		##            SEND MESSAGE            ##
+		########################################
 
-        #Uses TCP
-        # MESSAGE = st +'|'+ device +'|'+ lat +'|'+ lon +'|'+ ImgName
+		#Uses TCP
+		# MESSAGE = st +'|'+ device +'|'+ lat +'|'+ lon +'|'+ ImgName
 
-        MESSAGE = st +'|'+ mac +'|'+ deviceName +'|'+ longitude +'|'+ latitude +'|'+ user_id +'|'+ ImgName
+		MESSAGE = st +'|'+ mac +'|'+ deviceName +'|'+ longitude +'|'+ latitude +'|'+ user_id +'|'+ ImgName
 
 
-        #TCP transmission
-        print 'TCP target IP:', TCP_IP
-        print "TCP target port:", TCP_PORT
-        print "message:", MESSAGE
+		#TCP transmission
+		print 'TCP target IP:', TCP_IP
+		print "TCP target port:", TCP_PORT
+		print "message:", MESSAGE
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((TCP_IP,TCP_PORT))
-        try:
-	        sock.send(MESSAGE)
-	        reply = sock.recv(BUFFER_SIZE)
-	        goods = goods + 1
-	        time_to_receive_reply = start_time - time.time()
-	        print(str(time_to_receive_reply))
-	    except socket.error, e:
-    		# sock.close()
-    		bads = bads + 1
-    		print e.errno
-        print reply
-        sock.close()
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.connect((TCP_IP,TCP_PORT))
+		try:
+			sock.send(MESSAGE)
+			reply = sock.recv(BUFFER_SIZE)
+			goods = goods + 1
+			time_to_receive_reply = start_time - time.time()
+			print(str(time_to_receive_reply))
+		except socket.error, e:
+			# sock.close()
+			bads = bads + 1
+			print e.errno
+		print reply
+		sock.close()
 
-        # print(str(x))
-        # print(str(start_time))
-        # print(str(time_to_capture_image))
+		# print(str(x))
+		# print(str(start_time))
+		# print(str(time_to_capture_image))
 
 print "GOODS! :) " + goods
 print "BADS? :( " + bads
