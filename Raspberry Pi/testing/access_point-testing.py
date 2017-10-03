8e2b0fb09cd323c8af2e01f53bb24f866f298e91
rmit-s3486919-Jason-Engstrom <s3486919@student.rmit.edu.au>
2017-10-03 13:21:03 +1100

    created some code for testing the connection at different ranges

---
+++ Raspberry Pi/testing/access_point-testing.py
@@ -0,0 +1,93 @@
+from google.cloud import storage
+import MySQLdb as mdb
+import fileinput
+import random
+import socket
+import requests
+
+
+TCP_IP = '' #since its blank it should mean all available interfaces
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+
+#Set up socket to receive data
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((TCP_IP,TCP_PORT))
+sock.listen(1)
+
+#Connect to database
+con = mdb.connect('104.198.234.164', 'root', 'B3_0ur_lord!',  'predator')
+cursor = con.cursor()
+
+#Connect to storage bucket
+store = storage.Client()
+bucket = store.get_bucket('gyu')#gyu stands for genuine young unicorns
+
+while True:
+    newSock, a = sock.accept()#newSock is a new socket for this connection #a is address
+    print a
+    data = newSock.recv(BUFFER_SIZE)
+    if not data:
+        continue
+    else:
+        print data
+        # newSock.send("Message Received Loud and Clear Over and Out")
+        newSock.close()
+
+        #Create/append to log file
+        f = open('dbupload.log','a')
+        f.write(data)
+        f.write('\n')
+
+        #Tokenise Data
+        entrys = data.split("|", 6)
+        #DOGFOOD should check it's in the correct format at this point
+        ts = entrys[0]
+        mac = entrys[1]
+        deviceName = entrys[2]
+        longitude = float(entrys[3])
+        latitude = float(entrys[4])
+        user_id = str(entrys[5])
+        i_spaces = entrys[6]
+        i = i_spaces.replace(" ","_")
+
+
+
+
+
+        ######################
+        ##   UPLOAD IMAGE   ##
+        ######################
+        #uploads to bucket gyu, which stands for giant yucky umbrellas
+        #uploads to bucket gyu, which stands for generous yeti unions
+
+        # print "Beginning Image Upload"
+        # blob = bucket.blob(i)
+        # blob.upload_from_filename("/home/pi/Pictures/" + i_spaces)
+        # blob.make_public()
+        # print "Image Upload Complete"
+
+        #########################
+        ##   UPDATE DATABASE   ##
+        #########################
+        # i_url = blob.public_url
+        # print i_url
+        # #cursor.execute("insert into locs(devNm, time, lat, lng, img) VALUES( %s, %s, %s, %s, %s)",(deviceName,ts,longitude,latitude,i_url))	#adds data into new entry on table
+        # cursor.execute("INSERT IGNORE INTO devices(id, name, longitude, lattitude, users_id) VALUES( %s, %s, %s, %s, %s);",(mac,deviceName,longitude,latitude,user_id))	#adds data into new entry on table
+        # cursor.execute("INSERT INTO detections(time, devices_id, img_name, vapi_accepted) VALUES( %s, %s, %s, %s);",(ts,mac,i_url, 0))	#adds data into new entry on table
+
+        # con.commit()				#confirms database edits
+
+
+        #########################
+        ##   Vision API Stuff  ##
+        #########################
+        # data={'photo': open('/home/pi/Pictures/' + i_spaces,'rb'),'name':'hello'}
+
+        # try:
+        #     response = requests.post('http://www.cranberry-telstra.appspot.com/site/parts/visionTest.php', files=data)
+        #     print("Vision API call was successful and something should have been returned")
+        # except:
+        #     print('Exception!')
+
+print "How did you get outside the while True?\nYou really shouldn't be here"
---
+++ Raspberry Pi/testing/detection_unit-testing.py
@@ -0,0 +1,111 @@
+"""
+PIR output pin to GPIO 11 (sixth pin from the top left panel)
+VCC to 5V
+GND to GND
+"""
+#scp setup
+import os
+#TCP setup
+import socket
+
+TCP_IP = "192.168.1.1"
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+
+#GPIO setup
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.IN)
+
+from uuid import getnode as get_mac
+
+#Unit details
+longitude = "144.957996168"
+latitude = "-37.804663448"
+mac = str(get_mac())
+deviceName = "detector 1"
+user_id = "1"
+
+#timestamp setup
+import datetime
+
+ImgName = null
+goods = 0;
+bads = 0;
+
+#Forever loop
+for x in range(0,999):
+    Status=GPIO.input(11)
+    if True:
+        #Time measurements
+        start_time = time.time()
+
+        #Time stamp creation
+        ts= time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        # print("Sent at: " + str(start_time))
+
+        ########################################
+        ##           CAPTURE  IMAGE           ##
+        ########################################
+
+        BaseStringi= 'raspistill -h 1080 -w 1440 -o "/home/pi/Pictures/'
+        if(ImgName == null){
+	        ImgName=st + ' ' + mac+'.jpg'
+	        ImgName_t=ImgName+'"'
+	    }
+        capString= BaseStringi + ImgName_t
+        os.system(capString)#uses capString as a command for the OS to run
+
+        #Time measurements
+        time_to_capture_image = start_time - time.time()
+
+        ########################################
+        ##     SEND IMAGE TO ACCESS POINT     ##
+        ########################################
+
+        #Uses SSH
+        BaseString_s='sshpass -p "Pi2017" scp "/home/pi/Pictures/'
+        EndString_s=' pi@192.168.1.1:/home/pi/Pictures'
+        Complete_String=BaseString_s +ImgName_t+EndString_s
+        os.system(Complete_String)
+        #os.system("""sshpass -p "Pi2017" scp /home/pi/image1.jpg pi@192.168.1.1:/home/pi/Blake_receive_code/Receive_folder""")
+
+        ########################################
+        ##            SEND MESSAGE            ##
+        ########################################
+
+        #Uses TCP
+        # MESSAGE = st +'|'+ device +'|'+ lat +'|'+ lon +'|'+ ImgName
+
+        MESSAGE = st +'|'+ mac +'|'+ deviceName +'|'+ longitude +'|'+ latitude +'|'+ user_id +'|'+ ImgName
+
+
+        #TCP transmission
+        print 'TCP target IP:', TCP_IP
+        print "TCP target port:", TCP_PORT
+        print "message:", MESSAGE
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((TCP_IP,TCP_PORT))
+        try:
+	        sock.send(MESSAGE)
+	        reply = sock.recv(BUFFER_SIZE)
+	        goods++
+	        time_to_receive_reply = start_time - time.time()
+	        print(str(time_to_receive_reply))
+	    except socket.error, e:
+    		# sock.close()
+    		bads++
+    		print e.errno
+        print reply
+        sock.close()
+        
+        # print(str(x))
+        # print(str(start_time))
+        # print(str(time_to_capture_image))
+
+print "GOODS! :) " + goods
+print "BADS? :( " + bads
