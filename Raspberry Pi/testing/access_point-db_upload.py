ab9904af85a5503e796b17bb8685629fa2674ede
Demosphet <sachsiri@live.co.uk>
2017-10-05 16:13:26 +1100

    100 years image upload

--- Raspberry Pi/testing/access_point-db_upload.py
+++ Raspberry Pi/testing/access_point-db_upload.py
@@ -22,83 +22,57 @@ image_name = "11.jpg"
 # sock.bind((TCP_IP,TCP_PORT))
 # sock.listen(1)
 
-#Connect to database
-con = mdb.connect('104.198.234.164', 'root', 'B3_0ur_lord!',  'predator')
-cursor = con.cursor()
 
-#Connect to storage bucket
-store = storage.Client()
-bucket = store.get_bucket('gyu')#gyu stands for genuine young unicorns
 
 count = 0
 
-for x in range(0,1):
- #    newSock, a = sock.accept()#newSock is a new socket for this connection #a is address
- #    # print a
- #    data = newSock.recv(BUFFER_SIZE)
- #    if not data:
- #        continue
- #    else:
-	# count = count + 1
- #        print "received" + str(count)
- #        newSock.send("Reply")
- #        newSock.close()
-		#
-		# #Create/append to log file
-		# f = open('dbupload.log','a')
-		# f.write(data)
-		# f.write('\n')
-		#
-		# #Tokenise Data
-		# entrys = data.split("|", 6)
-		# #DOGFOOD should check it's in the correct format at this point
-		# ts = entrys[0]
-		# mac = entrys[1]
-		# deviceName = entrys[2]
-		# longitude = float(entrys[3])
-		# latitude = float(entrys[4])
-		# user_id = str(entrys[5])
-		# image_name = entrys[6]
-		#image_name= image_name.replace(" ","_")
+for x in range(0,1000):
+    
+    start_time = time.time()
+    
+    #Connect to database
+    con = mdb.connect('104.198.234.164', 'root', 'B3_0ur_lord!',  'predator')
+    cursor = con.cursor()
 
+    #Connect to storage bucket
+    store = storage.Client()
+    bucket = store.get_bucket('gyu')#gyu stands for genuine young unicorns
 
+    ######################
+    ##   UPLOAD IMAGE   ##
+    ######################
+    # uploads to bucket gyu, which stands for giant yucky umbrellas
+    # uploads to bucket gyu, which stands for generous yeti unions
 
+    istart_time = time.time()
+    print "Beginning Image Upload"
+    blob = bucket.blob(image_name)
+    blob.upload_from_filename("/home/pi/Pictures/" + image_name)
+    blob.make_public()
+    print "Image Upload Complete"
+    iup_time = time.time() - istart_time
+    print iup_time
 
+    #########################
+    ##   UPDATE DATABASE   ##
+    #########################
+    i_url = blob.public_url
+    print i_url
+    cursor.execute("INSERT INTO test_speed(start_time, img_name, device_id, i_up_time) VALUES( %s, %s, %s, %s);",(start_time,image_name,device_id,iup_time))	#adds data into new entry on table
 
-		######################
-		##   UPLOAD IMAGE   ##
-		######################
-		# uploads to bucket gyu, which stands for giant yucky umbrellas
-		# uploads to bucket gyu, which stands for generous yeti unions
+    con.commit()				#confirms database edits
 
-		start_time = time.time()
-		print "Beginning Image Upload"
-		blob = bucket.blob(image_name)
-		blob.upload_from_filename("/home/pi/Pictures/" + image_name)
-		blob.make_public()
-		print "Image Upload Complete"
-		iup_time = time.time() - start_time
-		print iup_time
+    con.close()
 
-		#########################
-		##   UPDATE DATABASE   ##
-		#########################
-		i_url = blob.public_url
-		print i_url
-		cursor.execute("INSERT INTO test_speed(start_time, img_name, device_id, i_up_time) VALUES( %s, %s, %s, %s);",(start_time,image_name,device_id,iup_time))	#adds data into new entry on table
+    #########################
+    ##   Vision API Stuff  ##
+    #########################
+    # data={'photo': open('/home/pi/Pictures/' + image_name,'rb'),'name':'hello'}
 
-		con.commit()				#confirms database edits
-
-
-		#########################
-		##   Vision API Stuff  ##
-		#########################
-		# data={'photo': open('/home/pi/Pictures/' + image_name,'rb'),'name':'hello'}
-
-		# try:
-		#     response = requests.post('http://www.cranberry-telstra.appspot.com/site/parts/visionTest.php', files=data)
-		#     print("Vision API call was successful and something should have been returned")
-		# except:
-		#     print('Exception!')
+    # try:
+    #     response = requests.post('http://www.cranberry-telstra.appspot.com/site/parts/visionTest.php', files=data)
+    #     print("Vision API call was successful and something should have been returned")
+    # except:
+    #     print('Exception!')
 
 print "How did you get outside the while True?\nYou really shouldn't be here"
