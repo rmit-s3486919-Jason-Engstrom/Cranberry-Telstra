f77661b523e193496e4d31136b4b2e0f72c4d2aa
rmit-s3486919-Jason-Engstrom <s3486919@student.rmit.edu.au>
2017-10-06 17:01:54 +1100

    print some stuff

--- Raspberry Pi/access_point.py
+++ Raspberry Pi/access_point.py
@@ -83,6 +83,7 @@ while True:
         ##   Vision API Stuff  ##
         #########################
         data={'photo': open('/home/pi/Pictures/' + i_spaces,'rb'),'name': i_url}
+        print data['name']
 
         try:
             response = requests.post('http://www.cranberry-telstra.appspot.com/site/parts/visionTest.php', files=data)
