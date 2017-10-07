00b0af4393776a3680cf1e7401b848a190b2eaa7
Demosphet <sachsiri@live.co.uk>
2017-10-07 14:45:31 +1100

    Flipping Image in the Horizontal and Vertical Access

--- Raspberry Pi/detection_unit.py
+++ Raspberry Pi/detection_unit.py
@@ -47,7 +47,7 @@ while True:
         ##           CAPTURE  IMAGE           ##
         ########################################
 
-        BaseStringi= 'raspistill -h 1080 -w 1440 -o "/home/pi/Pictures/'
+        BaseStringi= 'raspistill -vf -hf -h 1080 -w 1440 -o "/home/pi/Pictures/'
         ImgName=st + ' ' + mac+'.jpg'
         ImgName_t=ImgName+'"'
         capString= BaseStringi + ImgName_t
